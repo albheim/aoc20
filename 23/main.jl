cfe86a24f1260ce8c99244938008f7d5b494b84d
Albin Heimerson <albin.heimerson@control.lth.se>
2020-12-25 13:17:55 +0100

    much faster, not correct ans on 2 (should be same)

--- 23/main.jl
+++ 23/main.jl
@@ -1,14 +1,7 @@
 function runcupsgame(cups, iter)
     ptr = cups[1]
-    links = []
-    for i in 1:length(cups)
-        nidx = findfirst(==(i), cups) % length(cups) + 1
-        push!(links, cups[nidx])
-    end
+    links = cups[sortperm(cups) .% length(cups) .+ 1]
     for i in 1:iter
-        if i % 10000 == 0
-            @show i
-        end
         tripleidxs = [links[ptr], links[links[ptr]], links[links[links[ptr]]]]
 
         target = (ptr + length(links) - 2) % length(links) + 1
@@ -28,13 +21,12 @@ cups = parse.(Int, collect("$inp"))
 
 links = runcupsgame(cups, 100)
 
-ptr = 1
-for _ in eachindex(links)
+ptr = links[1]
+for _ in 2:length(links)
     print(findfirst(==(ptr), eachindex(links)))
     ptr = links[ptr]
 end
-println()
 
-links = runcupsgame([cups; maximum(cups)+1:1000000], 10000000)
+links = runcupsgame([cups; maximum(cups)+1:1_000_000], 10_000_000)
 
 @show links[1] * links[links[1]]
